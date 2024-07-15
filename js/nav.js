e6c51f88984051fc8fb7ac7cdc9a092e30ad6efa
Mohd. Salman Quadri <secretofsk@gmail.com>
2024-07-15 20:24:18 +0530

    new audios

--- js/nav.js
+++ js/nav.js
@@ -2,5 +2,4 @@ let sbar = document.querySelector(".searchsong");
 let sicon = document.querySelector(".searchicon");
 function tsbar() {
   sbar.classList.toggle("sbar");
-  sicon.classList.toggle("sbar");
 }
